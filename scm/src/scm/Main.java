89cb691d159612f856cb6b138372d5771249b31f
JemimaGra <jemima-grace.jemima-grace@capgemini.com>
2024-08-26 16:08:39 +0530

    scm

---
+++ scm/src/scm/Main.java
@@ -0,0 +1,11 @@
+package scm;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 String str="Hello";
+         System.out.println(str);
+	}
+
+}
